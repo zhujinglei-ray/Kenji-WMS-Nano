482487c7206bf5b64514a0ef6a023eece8d238bf
zhujinglei <48754396+zhujinglei@users.noreply.github.com>
2020-01-14 00:15:54 +0000

    'add-spring-security-as-login-system'

--- src/main/java/com/kenji/wms/controller/TestController.java
+++ src/main/java/com/kenji/wms/controller/TestController.java
@@ -1,34 +1,34 @@
-package com.kenji.wms.controller;
-
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class TestController {
-
-    @RequestMapping("/test")
-    public String test(){
-        return "test";
-    }
-
-    @RequestMapping("/404")
-    public String errorPage(){
-        return "404";
-    }
-
-    @RequestMapping("/login")
-    public String loginTest(){
-        return "login";
-    }
-
-    @RequestMapping("/tables")
-    public String loginTable(){
-        return "tables";
-    }
-
-    @RequestMapping("/")
-    public String index() {
-        return "index";
-    }
-}
+//package com.kenji.wms.controller;
+//
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class TestController {
+//
+//    @RequestMapping("/test")
+//    public String test(){
+//        return "test";
+//    }
+//
+//    @RequestMapping("/404")
+//    public String errorPage(){
+//        return "404";
+//    }
+//
+//    @RequestMapping("/login")
+//    public String loginTest(){
+//        return "login";
+//    }
+//
+//    @RequestMapping("/tables")
+//    public String loginTable(){
+//        return "tables";
+//    }
+//
+//    @RequestMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+//}
