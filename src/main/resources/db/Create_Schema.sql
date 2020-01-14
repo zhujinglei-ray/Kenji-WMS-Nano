b8e3a6e6774d5241cf56a4fbbee28075928bd547
Ruiheng Li <tikka0755@gmail.com>
2020-01-14 21:31:13 +0000

    sql table defination change

--- src/main/resources/db/Create_Schema.sql
+++ src/main/resources/db/Create_Schema.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS products;
 CREATE TABLE products(
     uuid               UUID PRIMARY KEY NOT NULL,
     product_id         BIGINT NOT NULL,
+    barcode            BIGINT NOT NULL,
     product_json       JSON NOT NULL
 );
 
@@ -16,23 +17,6 @@ CREATE TABLE productStock
   product_id         BIGINT NOT NULL,
   barcode            BIGINT NOT NULL,
   stock_json         JSON NOT NULL
-);
-
-DROP TABLE IF EXISTS products;
-CREATE TABLE products(
-                         uuid               UUID PRIMARY KEY NOT NULL,
-                         product_id         BIGINT NOT NULL,
-                         product_json       JSON NOT NULL
-);
-
-DROP TABLE IF EXISTS productStock;
-CREATE TABLE productStock
-(
-    uuid               UUID PRIMARY KEY NOT NULL,
-    stock_id           BIGINT NOT NULL,
-    product_id         BIGINT NOT NULL,
-    barcode            BIGINT NOT NULL,
-    stock_json         JSON NOT NULL
 );
 
 DROP TABLE IF EXISTS auth_user_role;
