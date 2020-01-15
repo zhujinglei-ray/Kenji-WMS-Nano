e3cf656733f9c0f86d336262b5958fea749c8e17
Ruiheng Li <tikka0755@gmail.com>
2020-01-15 21:43:30 +0000

    add json property

--- src/main/java/com/kenji/wms/stock/repository/PostgresStockRepository.java
+++ src/main/java/com/kenji/wms/stock/repository/PostgresStockRepository.java
@@ -37,7 +37,7 @@ public class PostgresStockRepository implements StockRepository {
     @Override
     public void updateBatchProductStocks(List<ProductStock> stocks) {
         String sql = "INSERT INTO productStock(stock_id, product_id, stock_json) VALUES(?, ?, ?::json) " +
-                "ON CONFLICT(stock_id) DO update set product_id=?, product_json=?::json;";
+                "ON CONFLICT(stock_id) DO update set product_id=?, stock_json=?::json;";
         jdbcTemplate.batchUpdate(sql, new batchProductStockUpdateSetter(stocks));
     }
 
