a265611aad7a3729edc1960e81fa0575496c12ee
Ruiheng Li <tikka0755@gmail.com>
2020-01-20 22:42:19 +0000

    jackson configure

--- src/main/java/com/kenji/wms/config/AppConfig.java
+++ src/main/java/com/kenji/wms/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.kenji.wms.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,4 +12,11 @@ public class AppConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper;
+    }
+
 }
